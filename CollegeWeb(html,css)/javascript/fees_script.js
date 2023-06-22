1217d71485a1672ce473f8c4ec3bc16ec5fab4ac
Bhavana Jain <135971916+BhavanaJain02@users.noreply.github.com>
2023-06-22 11:49:42 +0530

    Add files via upload

---
+++ CollegeWeb(html,css)/javascript/fees_script.js
@@ -0,0 +1,131 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==1){
+        document.getElementById("demo").innerHTML ="Bhavana Jain";
+        document.getElementById("demo1").innerHTML ="10000";
+    }
+    else if(inputVal==2)
+    {
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="Brindha A";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="Ashok H";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==5)
+    {
+        document.getElementById("demo").innerHTML ="Arjun";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==6)
+    {
+        document.getElementById("demo").innerHTML ="Bhavana Jain";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="Bhavana J";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="Bhavana singh";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==9)
+    {
+        document.getElementById("demo").innerHTML ="Bhavana CHOUDHARY";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA DASADIYA";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA GOMTIVAL";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==12)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA GUPTA";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA JUNAKIYA";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA KADIWALA";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA MAKNOJIYA";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA MOGAL";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA NAYI";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA PALASARA";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA PARMAR";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA PARMAR";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==21)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA ASHOKBHAI";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==22)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANAI PATEL";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==23)
+    {
+        document.getElementById("demo").innerHTML ="BHAVANA PATEL";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    
+    
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
+
